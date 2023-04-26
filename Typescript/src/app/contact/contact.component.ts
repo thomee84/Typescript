eb348877abb8b2394dd3f79bfab2d1c3ed7e6f2b
Tomi <Tomi@DESKTOP-8F1FU4F>
2023-04-26 21:20:43 +0200

    ver3

---
+++ Typescript/src/app/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  template: `
+    <p>
+      contact works!
+    </p>
+  `,
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+
+}
---
+++ Typescript/src/app/status/status.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  template: `
+    <p>
+      status works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class StatusComponent {
+
+}
